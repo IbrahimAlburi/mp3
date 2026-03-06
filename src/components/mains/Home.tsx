f545bc10dde385c41487126146484f2c5418c8de
IbrahimAlburi <ibrahimalburi04@gmail.com>
2026-03-06 15:28:10 -0500

    i think i fixed image not loading in homepage in vercel link

--- src/components/mains/Home.tsx
+++ src/components/mains/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <main>
             <h2>home</h2>
-            <img src="../../../public/ibrahim_pic.jpeg" alt="Ibrahim Alburi"/>
+            <img src="/ibrahim_pic.jpeg" alt="Ibrahim Alburi"/>
             <p>Computer Science student at Boston University with experience in computer graphics and algorithms.
                 I've been a student at Boston University for the past 3 years.</p>
         </main>
